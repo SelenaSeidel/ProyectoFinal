8c01231daaafd28098d15ad5990821a5ff3d7c10
NazarenaAranda06 <nazarenaaranda06@gmail.com>
2024-08-25 14:04:23 -0300

    modificacion de archivos

--- js/products.js
+++ js/products.js
@@ -1,85 +1,49 @@
-const productsData = {
-  "catID": 101,
-  "catName": "Autos",
-  "products": [
-      {
-          "id": 50921,
-          "name": "Chevrolet Onix Joy",
-          "description": "Generación 2019, variedad de colores. Motor 1.0, ideal para ciudad.",
-          "cost": 13500,
-          "currency": "USD",
-          "soldCount": 14,
-          "image": "img/prod50921_1.jpg"
-      },
-      {
-          "id": 50922,
-          "name": "Fiat Way",
-          "description": "La versión de Fiat que brinda confort y a un precio accesible.",
-          "cost": 14500,
-          "currency": "USD",
-          "soldCount": 52,
-          "image": "img/prod50922_1.jpg"
-      },
-      {
-          "id": 50923,
-          "name": "Suzuki Celerio",
-          "description": "Un auto que se ha ganado la buena fama por su economía con el combustible.",
-          "cost": 12500,
-          "currency": "USD",
-          "soldCount": 25,
-          "image": "img/prod50923_1.jpg"
-      },
-      {
-          "id": 50924,
-          "name": "Peugeot 208",
-          "description": "El modelo de auto que se sigue renovando y manteniendo su prestigio en comodidad.",
-          "cost": 15200,
-          "currency": "USD",
-          "soldCount": 17,
-          "image": "img/prod50924_1.jpg"
-      },
-      {
-          "id": 50925,
-          "name": "Bugatti Chiron",
-          "description": "El mejor hiperdeportivo de mundo. Producción limitada a 500 unidades.",
-          "cost": 3500000,
-          "currency": "USD",
-          "soldCount": 0,
-          "image": "img/prod50925_1.jpg"
-      }
-  ]
-};
-
 function renderProducts(products) {
   const productList = document.getElementById("carrousel");
   productList.innerHTML = ''; 
 
-  products.forEach((product, index) => {
-
-    let activo;
-    if (index === 0) {
-      activo = 'active';
-    } else {
-      activo = '';
-    }
-    
-    const productHTML = `
-      <div class="carousel-item ${activo}">
-          <img src="${product.image}" class="d-block w-100" alt="Imagen del ${product.name}">
-          <div class="carousel-caption d-none d-md-block">
-              <h4>${product.name}</h4>
-              <p>${product.description}</p>
-              <h5>${product.currency} ${product.cost}</h5>
-              <p>Vendidos: ${product.soldCount}</p>
+  products.forEach(product => {
+      const productHTML = `
+          <div class="swiper-slide">
+                <img src="${product.image}" alt="Imagen del ${product.name}">
+                <div class="swiper-slide-caption">
+                    <h4>${product.name}</h4>
+                    <p>${product.description}</p>
+                    <h5>${product.currency} ${product.cost}</h5>
+                    <p>Vendidos: ${product.soldCount}</p>
+                </div>
+              </div>
           </div>
-      </div>
       `;
 
       productList.innerHTML += productHTML;
   });
 
+  new Swiper('.swiper-container', {
+      effect: 'coverflow',
+      slidesPerView: 2,
+      centeredSlides: true,
+      initialSlide: 2,
+      coverflowEffect: {
+          rotate: 0,
+          stretch: 200,
+          depth: 100,
+          modifier: 1,
+          slideShadows: true,
+      },
+      pagination: {
+          el: '.swiper-pagination',
+          clickable: true,
+      },
+      navigation: {
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev',
+      },
+  });
 }
 
 document.addEventListener('DOMContentLoaded', () => {
-  renderProducts(productsData.products);
+  fetch("https://japceibal.github.io/emercado-api/cats_products/101.json")
+  .then(response => response.json())
+  .then(productsData => renderProducts(productsData.products));
 });
