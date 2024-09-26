0466ddde72b28670b58402e1c5b5ccee83490a22
NazarenaAranda06 <nazarenaaranda06@gmail.com>
2024-09-26 20:53:32 -0300

    related Products

--- js/product-info.js
+++ js/product-info.js
@@ -5,7 +5,6 @@ function renderProducts(product) {
   const desc = document.getElementById("Proddesc");
      
   titulo.innerHTML=`<h3>${product.category}</h3>`
-  desc.innerHTML=` <p>${product.description}</p>`
  
   for (const image of product.images){
     const productHTML = `
@@ -15,6 +14,7 @@ function renderProducts(product) {
                       <h4>${product.name}</h4>
                       <h5>${product.currency} ${product.cost}</h5>
                       <p>Vendidos: ${product.soldCount}</p>
+                      <p>${product.description}</p>
                   </div>
                 </div>
             </div>
@@ -46,9 +46,27 @@ function renderProducts(product) {
         prevEl: '.swiper-button-prev',
       },
     });
-}
 
   
+  const relatedProducts = document.querySelector("#related-products .carousel-inner");
+ 
+  product.relatedProducts.forEach((p, i) => {
+    const relatedProductElement = document.createElement("div")
+    relatedProductElement.classList.add("carousel-item")
+    const relatedProductImg = document.createElement("img")
+    relatedProductImg.setAttribute('src', p.image)
+    relatedProductElement.appendChild(relatedProductImg)
+
+    if(i == 0) {
+      relatedProductElement.classList.add("active")
+    }
+  
+    relatedProducts.appendChild(relatedProductElement);
+  })
+}
+
+
+
 document.addEventListener("DOMContentLoaded", function(e){
     let id=localStorage.getItem('productID')
     let url=PRODUCT_INFO_URL+id+".json"
