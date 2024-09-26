7a56cd33f26de79f46bf07e46261f70ba7525120
SelenaSeidel <selenaluisaseidel@gmail.com>
2024-09-26 20:12:46 -0300

    Mostrar comentarios y calificaciones

--- js/product-info.js
+++ js/product-info.js
@@ -45,18 +45,52 @@ function renderProducts(product) {
         nextEl: '.swiper-button-next',
         prevEl: '.swiper-button-prev',
       },
-    });
+  });
 }
 
+/*Parte que agrega comentarios y  */
+function comentarios(product){
+  const tabla=document.getElementById("tablacomentarios")
+    product.forEach(element => {
+        let cantidadestrellas=estrellas(element.score)
+        tabla.innerHTML+= `
+    <tr>
+        <td class="table-warning p-2 text-center">${element.user}</td>
+        <td class="table-default p-2">${element.description} <small> ${element.dateTime}</small></td>
+        <td class="table-warning p-2 text-center">${cantidadestrellas}</td>
+    </tr>`
+    })
+}
+
+function estrellas(numero){
+  var estrellas = "";
+  if (numero > 0) {
+    for (let i = 0; i < numero; i++) {
+      estrellas += `
+        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-star" viewBox="0 0 16 16">
+          <path d="M2.866 14.85c-.078.444.36.791.746.593l4.39-2.256 4.389 2.256c.386.198.824-.149.746-.592l-.83-4.73 3.522-3.356c.33-.314.16-.888-.282-.95l-4.898-.696L8.465.792a.513.513 0 0 0-.927 0L5.354 5.12l-4.898.696c-.441.062-.612.636-.283.95l3.523 3.356-.83 4.73zm4.905-2.767-3.686 1.894.694-3.957a.56.56 0 0 0-.163-.505L1.71 6.745l4.052-.576a.53.53 0 0 0 .393-.288L8 2.223l1.847 3.658a.53.53 0 0 0 .393.288l4.052.575-2.906 2.77a.56.56 0 0 0-.163.506l.694 3.957-3.686-1.894a.5.5 0 0 0-.461 0z"/>
+        </svg>
+      `;
+    }
+  }
+  return estrellas;
+}
   
 document.addEventListener("DOMContentLoaded", function(e){
     let id=localStorage.getItem('productID')
-    let url=PRODUCT_INFO_URL+id+".json"
-   getJSONData(url).then(function(resultObj){
+    let url_info=PRODUCT_INFO_URL+id+".json"
+    let url_coment=PRODUCT_INFO_COMMENTS_URL+id+".json"
+   getJSONData(url_info).then(function(resultObj){
           if (resultObj.status === "ok"){
               let producto= resultObj.data
               renderProducts(producto)
           }
-      });
+    });
+    getJSONData(url_coment).then(function(resultObj){
+      if (resultObj.status === "ok"){
+          let producto= resultObj.data
+          comentarios(producto)
+      }
+  });
   })
 
